b3f480f565c8cdb1f915fcbaa33279ff140a69b8
Brian Bogovich <bbogovich@smartops.com>
2013-01-20 13:59:18 -0500

    added node_modules to .gitignore

--- server.js
+++ server.js
@@ -1,26 +1,188 @@
-var app = require('http').createServer(handler)
-, io = require('socket.io').listen(app)
-, fs = require('fs')
+var System = require("util");
+var fs = require('fs');
+var HTTP = require("http");
 
-app.listen(process.env.PORT || 8001);
+var WebSocketServer = require("websocket").server;
+var index = fs.readFileSync('index.html');
+//var gameEngine = require("./GameEngine");
 
-function handler (req, res) {
-fs.readFile('index.html',
-function (err, data) {
-if (err) {
-res.writeHead(500);
-return res.end('Error loading index.html');
+var MaxConnections = 10;
+var Connections = {};
+
+function ObjectSize(Obj)
+{
+    var Size = 0;
+	for (var Key in Obj)
+		if (Obj.hasOwnProperty(Key))
+			Size++;
+	return Size;
 }
 
-res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
-res.end(data);
-});
+function SendGameState()
+{
+    /*
+	var messagePlayers = [];
+	var messageMissiles = [];
+	var players = gameEngine.gameState.players;
+	var missiles = gameEngine.gameState.missiles;
+	for (var i in players){
+		messagePlayers.push(players[i]);
+	}
+	for (i in missiles){
+		messageMissiles.push(missiles[i]);
+	}
+	for (var ID in Connections){
+		Connections[ID].sendUTF(JSON.stringify({
+				transactionId:new Date().getTime(),
+				messageType:"game.defaultgame.outbound.GameStateMessage",
+				players:messagePlayers,
+				missiles:messageMissiles,
+				paused:gameEngine.gameState.paused,
+				started:gameEngine.gameState.started
+			}));
+	}
+    */
 }
 
-io.sockets.on('connection', function (socket) {
-// echo the message
-socket.on('message', function (data) {
-console.info(data);
-socket.send("[ECHO] "+data);
-});
-});
+/*
+ * When a websocket disconnects
+ * */
+function HandleClientClosure(ID)
+{
+	if (ID in Connections)
+	{
+		System.log("Disconnect from " + Connections[ID].IP);
+		delete Connections[ID];
+		gameEngine.removePlayer(ID);
+	}
+}
+
+function HandleClientMessage(ID, Message)
+{
+	// Check that we know this client ID and that the message is in a format we expect.
+	if (!(ID in Connections)) return;
+	System.log("HandleClientMessage("+ID+","+Message+") ");
+	if(Connections[ID]){
+		var connection = Connections[ID];
+		connection.sendUTF(JSON.stringify({
+				transactionId:new Date().getTime(),
+				messageType:"ACK",
+				rxId:Message.transactionId
+		}));
+		if(Message.messageType){
+			switch(Message.messageType){
+				case "game.inbound.GameRegistrationMessage":
+					//gameEngine.initPlayer(ID);
+					connection.sendUTF(JSON.stringify({
+							transactionId:new Date().getTime(),
+							messageType:"game.outbound.RegistrationSuccessMessage",
+							sessionId:connection.ID
+						}));
+					break;
+				default:
+					//gameEngine.addPlayerMessage(ID,Message);
+					break;
+			}
+		}
+	}
+	System.log("HandleClientMessage: Exiting");
+}
+
+//Creates an HTTP server that will respond with a simple blank page when accessed.
+var HTTPServer = HTTP.createServer(
+		function(Request, Response) {
+			System.log(Request);
+			for (var i in Request){
+				if(typeof(Request[i])!="function"){
+					System.log(i+": "+(typeof(i)!="function"?Request[i]:"[function Function]"));
+				}
+			}
+			var content;
+			try{
+				var url=Request.url;
+				if(url=="/"){
+					url="/index.html";
+				}
+				content = fs.readFileSync(url.replace(/^\//,""));
+				var urlMatch = url.match(/\.([^$]+)$/);
+				var contentType="text/plain";
+				if(urlMatch!==null){
+					switch(urlMatch[1].toLowerCase()){
+						case "html":
+							contentType="text/html";
+							break;
+						case "js":
+							contentType="application/javascript";
+							break;
+					}
+				}
+				Response.writeHead(200, { "Content-Type": contentType });
+			}catch(e){
+				content="404!!!";
+				Response.writeHead(404, { "Content-Type": "text/plain" });
+			}
+			Response.end(content);
+		}
+	);
+
+// Starts the HTTP server on port 9001.
+HTTPServer.listen(9001, function() {
+		System.log("Listening for connections on port 9001"); 
+	}
+);
+
+// Creates a WebSocketServer using the HTTP server just created.
+var Server = new WebSocketServer(
+	{
+		httpServer: HTTPServer,
+		closeTimeout: 2000
+	}
+);
+
+function handleWebSocketRequest(Request){
+	if (ObjectSize(Connections) >= MaxConnections) {
+		Request.reject();
+		return;
+	}
+
+	var Connection = Request.accept(null, Request.origin);
+	Connection.IP = Request.remoteAddress;
+
+	// Assign a random ID that hasn't already been taken.
+	do {
+		Connection.ID = Math.floor(Math.random() * 100000);
+	} while (Connection.ID in Connections);
+
+	Connections[Connection.ID] = Connection;
+	Connection.sendUTF(JSON.stringify({
+				transactionId:new Date().getTime(),
+				messageType:"game.outbound.SessionCreatedMessage",
+				sessionId:Connection.ID
+			}));
+	Connection.on("message",
+			function(Message){
+				System.log("onMessage:"+Message);
+				for (var i in Message){
+					System.log(i+": "+Message[i]);
+				}
+				// All of our messages will be transmitted as unicode text.
+				if (Message.type == "utf8")
+					HandleClientMessage(Connection.ID, JSON.parse(Message.utf8Data));
+			}
+	);
+	Connection.on("close",function(){
+			HandleClientClosure(Connection.ID);
+		}
+	);
+	System.log("Logged in " + Connection.IP + "; currently " + ObjectSize(Connections) + " users.");
+};
+//When a client connects...
+Server.on("request",handleWebSocketRequest);
+
+function gameRunner(){
+	//if(gameEngine.runGameFrame()){
+	//	SendGameState();
+	//}
+	setTimeout(gameRunner,10/*gameEngine.FRAME_INTERVAL*/);
+}
+gameRunner();
